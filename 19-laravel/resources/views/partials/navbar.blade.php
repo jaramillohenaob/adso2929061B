16e857358e5975dfc8f8af71d365ee055eb75ec3
jaramillohenaob <jaramillohenaob@gmail.com>
2026-03-18 21:05:45 -0500

    Add user management features with controllers and views for users, pets, and adoption

--- 19-laravel/resources/views/partials/navbar.blade.php
+++ 19-laravel/resources/views/partials/navbar.blade.php
@@ -27,7 +27,13 @@
                 <img src="{{ asset('images/users/'.Auth::user()->photo)}}" class="w-12 h-12 object-cover rounded-full border-2">
                 {{ Auth::user()->fullname}}
             </summary>
-            <ul class="bg-black/60 rounded-t-none m-0 w-48 py-4 -ms-7">
+            <ul class="bg-black/60 rounded-t-none m-0 w-48 -ms-7 absolute right-0">
+                <li class="hover:bg-white/20 rounded-md">
+                    <a href="{{ url('dashboard')}}">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="size-5" fill="currentColor" viewBox="0 0 256 256"><path d="M104,40H56A16,16,0,0,0,40,56v48a16,16,0,0,0,16,16h48a16,16,0,0,0,16-16V56A16,16,0,0,0,104,40Zm0,64H56V56h48v48Zm96-64H152a16,16,0,0,0-16,16v48a16,16,0,0,0,16,16h48a16,16,0,0,0,16-16V56A16,16,0,0,0,200,40Zm0,64H152V56h48v48Zm-96,32H56a16,16,0,0,0-16,16v48a16,16,0,0,0,16,16h48a16,16,0,0,0,16-16V152A16,16,0,0,0,104,136Zm0,64H56V152h48v48Zm96-64H152a16,16,0,0,0-16,16v48a16,16,0,0,0,16,16h48a16,16,0,0,0,16-16V152A16,16,0,0,0,200,136Zm0,64H152V152h48v48Z"></path></svg>
+                        Dashboard
+                    </a>
+                </li>
                 <li class="hover:bg-white/20 rounded-md">
                     <a href="{{ url('users')}}">
                         <svg xmlns="http://www.w3.org/2000/svg" class="size-5" fill="currentColor" viewBox="0 0 256 256"><path d="M117.25,157.92a60,60,0,1,0-66.5,0A95.83,95.83,0,0,0,3.53,195.63a8,8,0,1,0,13.4,8.74,80,80,0,0,1,134.14,0,8,8,0,0,0,13.4-8.74A95.83,95.83,0,0,0,117.25,157.92ZM40,108a44,44,0,1,1,44,44A44.05,44.05,0,0,1,40,108Zm210.14,98.7a8,8,0,0,1-11.07-2.33A79.83,79.83,0,0,0,172,168a8,8,0,0,1,0-16,44,44,0,1,0-16.34-84.87,8,8,0,1,1-5.94-14.85,60,60,0,0,1,55.53,105.64,95.83,95.83,0,0,1,47.22,37.71A8,8,0,0,1,250.14,206.7Z"></path></svg>
--- 19-laravel/routes/web.php
+++ 19-laravel/routes/web.php
@@ -4,6 +4,10 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Carbon\Carbon;
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PetController;
+use App\Http\Controllers\AdoptionController;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -88,6 +92,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// Resources
+Route::middleware('auth')->group( function () {
+    Route::resources([
+        'users'    => UserController::class,
+        'pets'     => PetController::class,
+        'adoption' => AdoptionController::class
+    ]);
 });
 
 require __DIR__.'/auth.php';
